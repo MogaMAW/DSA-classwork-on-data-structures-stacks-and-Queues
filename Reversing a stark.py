5df6321a49d40d939150f38a5986f4354a67f5fb
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-04-19 02:55:10 +1000

    Update Reversing a stark.py

--- Reversing a stark.py
+++ Reversing a stark.py
@@ -24,6 +24,7 @@ class Stack():
 PROMPT = "Enter an int value (<0 to end):"
 myStack = Stack()
 value = int(input( PROMPT ))
+
 while value >= 0 :
     myStack.push( value )
     value = int(input( PROMPT ))
